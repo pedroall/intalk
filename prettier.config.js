bbdfffc7c28994d65951aab7ecffcd715ebd25df
pedroall <cttpedroall@gmail.com>
2024-10-27 05:33:53 +0000

    use defaults

--- prettier.config.js
+++ prettier.config.js
@@ -1,13 +1,1 @@
-// prettier.config.js, .prettierrc.js, prettier.config.cjs, or .prettierrc.cjs
-
-/**
- * @see https://prettier.io/docs/en/configuration.html
- * @type {import("prettier").Config}
- */
-const config = {
-  tabWidth: 4,
-  semi: false,
-  singleQuote: true,
-};
-
-module.exports = config;
+module.exports = require('./defaults/base-prettier.config')
